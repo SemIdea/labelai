6901541a34582b69021423d12c7ac2ffe4460897
SemIdeia <codorkman@gmail.com>
2024-12-03 17:22:53 -0300

    new box logic

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,4 +1,5 @@
 "use client";
+import crc32 from "crc-32"; // Import crc-32 library
 
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
@@ -6,14 +7,7 @@ import { useRouter } from "next/navigation";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ThemeProviderProps } from "next-themes/dist/types";
 import { createContext } from "react";
-import crc32 from "crc-32"; // Import crc-32 library
-import {
-  Dropdown,
-  DropdownTrigger,
-  DropdownMenu,
-  DropdownItem,
-  Button,
-} from "@nextui-org/react";
+import { BoxI, FileContextI, ImageI, LabelI } from "./providers/types";
 
 export interface ProvidersProps {
   children: React.ReactNode;
@@ -30,58 +24,12 @@ export function Providers({ children, themeProps }: ProvidersProps) {
   );
 }
 
-export type Box = {
-  imageId: number;
-  labelId: number | null;
-  cords: {
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-  };
-  hover: boolean;
-  selected: boolean;
-  visible: boolean;
-};
-
-export type Label = {
-  id: number;
-  name: string;
-  color: string;
-};
-
-interface Image {
-  id: number;
-  url: string;
-  name: string;
-}
-
-interface FileContextType {
-  images: Image[];
-  handleImageUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  setImages: React.Dispatch<React.SetStateAction<Image[]>>;
-  boxes: Box[];
-  setBoxes: React.Dispatch<React.SetStateAction<Box[]>>;
-  labels: Label[];
-  setLabels: React.Dispatch<React.SetStateAction<Label[]>>;
-  handleLabelSelect: (boxId: number, labelId: number) => void;
-}
-
-const FileContext = createContext<FileContextType | undefined>(undefined);
-
-// export async function crc32(message: string) {
-//   const msgBuffer = new TextEncoder().encode(message);
-//   const hashBuffer = await crypto.subtle.digest("crc32", msgBuffer);
-//   const hashArray = Array.from(new Uint8Array(hashBuffer));
-//   const hashHex = hashArray
-//     .map((b) => ("00" + b.toString(16)).slice(-2))
-//     .join("");
-//   return hashHex;
-// }
+const FileContext = createContext<FileContextI | undefined>(undefined);
 
 export const FileProvider = ({ children }: { children: React.ReactNode }) => {
-  const [boxes, setBoxes] = React.useState<Box[]>([]);
-  const [labels, setLabels] = React.useState<Label[]>([
+  const [currentBoxes, setCurrentBoxes] = React.useState<BoxI[]>([]);
+  const [currentLabel, setCurrentLabel] = React.useState<LabelI | null>(null);
+  const [labels, setLabels] = React.useState<LabelI[]>([
     {
       id: 1,
       name: "Car",
@@ -93,21 +41,23 @@ export const FileProvider = ({ children }: { children: React.ReactNode }) => {
       color: "ffffff",
     },
   ]);
-  const [images, setImages] = React.useState<Image[]>([
-    // {
-    //   url: "https://images.unsplash.com/photo-1546593064-053d21199be1?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //   id: crc32.str(
-    //     "https://images.unsplash.com/photo-1546593064-053d21199be1?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    //   ),
-    //   name: "Image1",
-    // },
-    // {
-    //   url: "https://images.unsplash.com/photo-1547567696-3fb538726ffd?q=80&w=1990&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //   id: crc32.str(
-    //     "https://images.unsplash.com/photo-1547567696-3fb538726ffd?q=80&w=1990&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-    //   ),
-    //   name: "Image2",
-    // },
+  const [images, setImages] = React.useState<ImageI[]>([
+    {
+      url: "https://images.unsplash.com/photo-1546593064-053d21199be1?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id: crc32.str(
+        "https://images.unsplash.com/photo-1546593064-053d21199be1?q=80&w=1975&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ),
+      name: "Image1",
+      boxes: [],
+    },
+    {
+      url: "https://images.unsplash.com/photo-1547567696-3fb538726ffd?q=80&w=1990&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      id: crc32.str(
+        "https://images.unsplash.com/photo-1547567696-3fb538726ffd?q=80&w=1990&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      ),
+      name: "Image2",
+      boxes: [],
+    },
   ]);
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -116,39 +66,23 @@ export const FileProvider = ({ children }: { children: React.ReactNode }) => {
       url: URL.createObjectURL(file),
       id: crc32.str(file.name),
       name: file.name,
+      boxes: [],
     }));
     setImages((prevImages) => [...prevImages, ...newImages]);
-    const newBoxes = files.map((file) => ({
-      imageId: crc32.str(file.name),
-      labelId: null,
-      color: "",
-      cords: { x1: 0, y1: 0, x2: 0, y2: 0 },
-      hover: false,
-      selected: false,
-      visible: true,
-    }));
-    setBoxes((prevBoxes) => [...prevBoxes, ...newBoxes]);
-  };
-
-  const handleLabelSelect = (boxId: number, labelId: number) => {
-    setBoxes((prevBoxes) =>
-      prevBoxes.map((box) =>
-        box.imageId === boxId ? { ...box, labelId } : box
-      )
-    );
   };
 
   return (
     <FileContext.Provider
       value={{
         images,
+        labels,
+        currentBoxes,
+        currentLabel,
+        setCurrentLabel,
+        setCurrentBoxes,
         handleImageUpload,
         setImages,
-        boxes,
-        setBoxes,
-        labels,
         setLabels,
-        handleLabelSelect,
       }}
     >
       {children}
@@ -162,26 +96,4 @@ export const useFileContext = () => {
     throw new Error("useFileContext must be used within a FileProvider");
   }
   return context;
-};
-
-export const LabelDropdown = ({ boxId }: { boxId: number }) => {
-  const { labels, handleLabelSelect } = useFileContext();
-
-  return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button variant="bordered">Select Label</Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Label Selection" items={labels}>
-        {(label) => (
-          <DropdownItem
-            key={label.id}
-            onClick={() => handleLabelSelect(boxId, label.id)}
-          >
-            {label.name}
-          </DropdownItem>
-        )}
-      </DropdownMenu>
-    </Dropdown>
-  );
 };
--- app/providers/types.ts
+++ app/providers/types.ts
@@ -1,9 +1,47 @@
+interface BoxI {
+  imageId: number;
+  labelId: number | null;
+  cords: ImageCordinatesI;
+  isHovered: boolean;
+  isSelected: boolean;
+  isVisible: boolean;
+}
 
-export type Point = {
+interface LabelI {
+  id: number;
+  name: string;
+  color: string;
+}
+
+interface ImageI {
+  id: number;
+  url: string;
+  name: string;
+  boxes: BoxI[];
+}
+
+interface FileContextI {
+  images: ImageI[];
+  labels: LabelI[];
+  currentBoxes: BoxI[];
+  currentLabel: LabelI | null;
+  setCurrentLabel: React.Dispatch<React.SetStateAction<LabelI | null>>;
+  setCurrentBoxes: React.Dispatch<React.SetStateAction<BoxI[]>>;
+  setImages: React.Dispatch<React.SetStateAction<ImageI[]>>;
+  setLabels: React.Dispatch<React.SetStateAction<LabelI[]>>;
+  handleImageUpload: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+interface ImageCordinatesI {
+  x1: number;
+  y1: number;
+  x2: number;
+  y2: number;
+}
+
+interface Point {
   x: number;
   y: number;
-};
+}
 
-export type CanvasOperation = "idle" | "drawing" | "moving" | "resizing";
-
-// ... existing types ...
+export type { BoxI, LabelI, ImageI, FileContextI, ImageCordinatesI, Point };
